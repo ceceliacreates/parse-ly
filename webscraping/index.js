9b89d5494e16034a93e57fc02a0e7593bbe60e89
kthor93 <teahorsen@gmail.com>
2019-05-30 14:23:13 -0400

    more testing

--- webscraping/index.js
+++ webscraping/index.js
@@ -1,16 +1,19 @@
 const cheerio = require('cheerio');
 const Nightmare = require('nightmare');
+const fs = require('fs');
 
 const nightmare = Nightmare({ show: true });
 
 let pageNumber = 1;
-let url = "https://www.houseplant411.com/houseplant/page/" + pageNumber;
+let url = 'https://www.houseplant411.com/houseplant/page/' + pageNumber;
 
 nightmare
     .goto(url)
     .wait('body')
+    .click('.resultName a')
+    .wait('body')
     .evaluate(() => document.querySelector('body').innerHTML)
-    .click('.resultsInd a')
+    .back()
     .end()
     .then(response => {
         console.log(getData(response));
@@ -19,15 +22,32 @@ nightmare
     });
 
 let getData = html => {
-    data = [];
     const $ = cheerio.load(html);
+    let data = [];
+    let care = [];
 
-    $(".resultsMid").each((i, element) => {
+    $(".post-meta-value").each((i, element) => {
+        care.push($(element).text());
+    });
+
+    $("#bodycontent").each((i, element) => {
         data.push({
-            title: $(element).find(".resultName").text(),
-            image: $(element).find("img").attr("src"),
-            link: $(element).find("a").attr("href")
+            commonName: $(element).find(".title").text(),
+            scientificName: $(element).find(".resultSpecies").text(),
+            lightNeeds: care[0],
+            waterNeeds: care[1],
+            tempNeeds: `${care[3]} ${care[4]}`,
+            soilNeeds: care[8],
+            potSize: care[9],
+            poisonous: care[13]
         });
+    });
+
+    // let plants = {}
+    // let plantsData = JSON.parse(JSON.stringify(data, null, 2))
+
+    fs.writeFile('plants.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) throw err;
     });
 
     return data;
