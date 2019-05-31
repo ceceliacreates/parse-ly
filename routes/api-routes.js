3ac0951d0856cf9f2e1149d59b0c8eb7534f2060
fyeh0 <fangiyeh@gmail.com>
2019-05-31 13:54:45 -0400

    add question to survey, update readme

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,5 @@
 var db = require("../models");
 
-// "Plant" db name can be changed, placeholder db name
 module.exports = function(app) {
     app.get("/api/plants", function(req, res) {
         db.Plant.findAll().then(function(data) {
