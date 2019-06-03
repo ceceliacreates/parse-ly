c12e41bb5bfa1d16cfd2cc7ffed08a725afc7be8
kthor93 <teahorsen@gmail.com>
2019-06-03 12:03:13 -0400

    update

--- seeds/seed-db.js
+++ seeds/seed-db.js
@@ -2,12 +2,12 @@ const db = require("../models");
 const fs = require("fs");
 const path = require('path')
 
-let data = fs.readFileSync(path.join(__dirname, "/transplants.json"));
-let transplants = JSON.parse(data);
+let data = fs.readFileSync(path.join(__dirname, "/plants.json"));
+let plants = JSON.parse(data);
 console.log(data)
 
 db.sequelize.sync({ force: true }).then(function () {
-    db.Plant.bulkCreate(transplants).then(function () {
+    db.Plant.bulkCreate(plants).then(function () {
         console.log("seeded");
     }).catch(function (err) {
         console.log(err);
