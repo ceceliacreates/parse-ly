d5f0fa9a32d8534742e65e7ec17feccea0faeac0
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-06-04 12:57:24 -0400

    survey updates

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ $("#survey").on("click", "button", function(event) {
       name = $("#name").val();
       $("#survey").empty();
       $("#survey").append(
-        `<h4>Hi ${name}! Do you have any pets?</h4><label for="yes">Yes </label><input type="radio" name="hasPets" id="yes" value="yes"><label for="no">No </label><input type="radio" name="hasPets" id="no" value="no"><button id="question3">Next</button>`
+        `<h4>Hi ${name}! Do you have any pets?</h4><label for="yes">Yes</label> <input type="radio" name="hasPets" id="yes" value="yes"><label for="no">No </label><input type="radio" name="hasPets" id="no" value="no"><button id="question3">Next</button>`
       );
       break;
     case "question3":
