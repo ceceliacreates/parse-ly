054a78bf70c640087265b9263916436a5e405fde
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-06-04 13:24:47 -0400

    updates survey values to match database columns

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 $(function() {
   //declares variables for survey values globally
   let name;
-  let hasPets;
+  let isPoisonous;
   let difficulty;
+  let diffRangeStart;
+  let diffRangeEnd;
   let temperature;
   let light;
   let plantType;
@@ -17,28 +19,42 @@ $(function() {
         name = $('#name').val();
         $('#survey').empty();
         $('#survey').append(
-          `<h4>Hi ${name}! Do you have any pets?</h4><label for="yes">Yes </label><input type="radio" name="hasPets" id="yes" value="yes"><label for="no">No </label><input type="radio" name="hasPets" id="no" value="no"><button id="question3">Next</button>`
+          `<h4>Hi ${name}! Do you have any pets?</h4><label for="yes">Yes </label><input type="radio" name="hasPets" id="yes" value="false"><label for="no">No </label><input type="radio" name="hasPets" id="no" value="true"><button id="question3">Next</button>`
         );
         break;
       case 'question3':
-        hasPets = $('input:checked').val();
+        isPoisonous = $('input:checked').val();
         $('#survey').empty();
         $('#survey').append(
-          `<h4>What kind of challenge are you looking for?</h4><label for="easy">Easy </label><input type="radio" name="difficulty" id="easy" value="easy"><label for="moderate">Moderate </label><input type="radio" name="difficulty" id="moderate" value="moderate"><label for="difficult">Difficult </label><input type="radio" name="difficulty" id="difficult" value="difficult"><button id="question4">Next</button>` // pictures/icons to represent difficulty here
+          `<h4>What kind of challenge are you looking for?</h4><label for="easy">Easy </label><input type="radio" name="difficulty" id="easy" value="1"><label for="moderate">Moderate </label><input type="radio" name="difficulty" id="moderate" value="moderate"><label for="difficult">Difficult </label><input type="radio" name="difficulty" id="difficult" value="difficult"><button id="question4">Next</button>` // pictures/icons to represent difficulty here
         );
         break;
       case 'question4':
         difficulty = $('input:checked').val();
+        switch (difficulty) {
+          case easy: 
+          diffRangeStart = 3;
+          diffRangeEnd = 5;
+          break;
+          case moderate:
+            diffRangeStart = 6;
+            diffRangeEnd = 8;
+            break;
+            case difficult:
+              diffRangeStart = 9;
+              diffRangeEnd = 9;
+              break;
+        }
         $('#survey').empty();
         $('#survey').append(
-          `<h4>At what temperature do you keep your home?</h4><label for="lt60">Less than 60&deg; </label><input type="radio" name="temperature" id="lt60" value="less than 60"><label for="60-70">Between 60&deg; - 70&deg; </label><input type="radio" name="temperature" id="60-70" value="between 60-70"><label for="gt70">Greater than 70&deg; </label><input type="radio" name="temperature" id="gt70" value="greater than 70"><button id="question5">Next</button>`
+          `<h4>At what temperature do you keep your home?</h4><label for="lt60">Less than 65&deg; </label><input type="radio" name="temperature" id="lt60" value="cold"><label for="60-70">Between 65&deg; - 75&deg; </label><input type="radio" name="temperature" id="60-70" value="cool"><label for="gt70">Greater than 75&deg; </label><input type="radio" name="temperature" id="gt70" value="warm"><button id="question5">Next</button>`
         );
         break;
       case 'question5':
         temperature = $('input:checked').val();
         $('#survey').empty();
         $('#survey').append(
-          `<h4>How much natural light is there in your home?</h4><label for="lowLight">Low Light </label><input type="radio" name="light" id="lowLight" value="low light"><label for="medium Light">Medium Light</label><input type="radio" name="light" id="medium Light" value="medium Light"><label for="highLight">High Light </label><input type="radio" name="light" id="highLight" value="high light"><button id="question6">Next</button>`
+          `<h4>How much natural light is there in your home?</h4><label for="lowLight">Low Light </label><input type="radio" name="light" id="lowLight" value="low"><label for="medium Light">Medium Light</label><input type="radio" name="light" id="medium Light" value="medium"><label for="highLight">High Light </label><input type="radio" name="light" id="highLight" value="high"><button id="question6">Next</button>`
         );
         break;
       case 'question6':
@@ -71,10 +87,11 @@ $(function() {
         potSize = $('input:checked').val();
         $('#survey').empty();
         let request = {
-          hasPets: hasPets,
-          difficulty: difficulty,
-          temperature: temperature,
-          light: light,
+          "isPoisonous": isPoisonous,
+          "diffRangeStart": diffRangeStart,
+          "diffRangeEnd": diffRangeEnd,
+          "temperature": temperature,
+          "light": light,
           plantType: plantType,
           potSize: potSize
         };
